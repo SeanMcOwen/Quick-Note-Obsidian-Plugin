0daef001095b79cdc9b62d312c2108572cbec9a8
SeanMcOwen <seanmcowen@gmail.com>
2024-08-18 12:15:16 -0400

    Checkpoint

--- main.ts
+++ main.ts
@@ -10,6 +10,8 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
 }
 
+
+
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 	notes: TFile[]
@@ -25,8 +27,11 @@ export default class MyPlugin extends Plugin {
 			}
 
 
+
+
 			const linkedFiles = Object.entries(this.app.metadataCache.resolvedLinks).filter(([_, value]) => Object.keys(value).contains(activeFile.name)).map(([key, _]) => key)
-			
+			const unresolvedLinks = this.app.metadataCache.unresolvedLinks;
+			console.log(unresolvedLinks)
 			
 			let aliases = linkedFiles.map( (file) => {
 				const cache = this.app.metadataCache.getCache(file)
@@ -93,14 +98,7 @@ export default class MyPlugin extends Plugin {
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
+
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
 			id: 'sample-editor-command',
@@ -108,27 +106,10 @@ export default class MyPlugin extends Plugin {
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				console.log(editor.getSelection());
 				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
 			}
 		});
+
 		setTimeout(() => this.notes = this.getAllNotes(), 200)
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
@@ -415,23 +396,6 @@ class SolidifyLinkModal extends Modal {
 	}
 }
 
-
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
 
 class SampleSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
