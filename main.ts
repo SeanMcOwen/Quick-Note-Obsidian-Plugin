50020718045951fab0ad5c5d1c89f4ec339a7ef9
SeanMcOwen <seanmcowen@gmail.com>
2024-08-19 20:09:49 -0400

    Setting

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
-import { App, Editor, MarkdownView, Plugin, PluginSettingTab, Setting, TAbstractFile, TFile } from 'obsidian';
+import { Editor, MarkdownView, Plugin, TAbstractFile, TFile } from 'obsidian';
 import { UnusedAliasModal, SolidifyLinkModal, AliasLinkModal } from "./ui/modal";
+import {SettingTab} from "./ui/setting"
 
 // Remember to rename these classes and interfaces!
 
@@ -114,7 +115,7 @@ export default class MyPlugin extends Plugin {
 		setTimeout(() => this.notes = this.getAllNotes(), 200)
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new SettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -178,28 +179,4 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
